fe480e8597cd0ee6f0f73ac717ee1b0cdc58c9b2
knight <knight@nautilusgames.com>
2025-09-19 01:47:53 +0700

    1809 new pre train model

--- detect_text.py
+++ detect_text.py
@@ -4,12 +4,14 @@ model = YOLO("yolov8n.pt")
 
 results = model.train(
     data="data_set/data.yaml",
-    epochs=5,         # giảm số epoch
-    imgsz=640,
-    batch=4,           # batch nhỏ vì data ít
+    epochs=30,          # tăng nhẹ epoch (15 có thể hơi ít, 30–50 hợp lý hơn)
+    imgsz=640,          # giữ nguyên, YOLO mặc định
+    batch=2,            # giảm batch vì dataset nhỏ, giúp cập nhật gradient mượt hơn
     name="text_detector_small",
-    plots=True         # xuất biểu đồ loss, mAP, P, R
+    plots=True,         # xuất biểu đồ loss, mAP, P, R
+    patience=10         # early stopping: dừng nếu 10 epoch liên tiếp không cải thiện
 )
+
 
 metrics = model.val()
 print(metrics)  # sẽ in Precision, Recall, mAP50, mAP50-95
