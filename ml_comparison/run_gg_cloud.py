1bfa2b2b09687491fbe97162697ef87a6dae22dd
knight <knight@nautilusgames.com>
2025-09-20 02:59:26 +0700

    2009 fix bug

--- ml_comparison/run_gg_cloud.py
+++ ml_comparison/run_gg_cloud.py
@@ -1,24 +1,20 @@
-from google.cloud import vision
+from main_gcv import YOLOGoogleVisionPipeline
+from main_ocr import SimpleOCRProcessor
 
-def detect_text(path):
-    client = vision.ImageAnnotatorClient()
+if __name__ == "__main__":
+    processor = SimpleOCRProcessor()
+    test_image = "../test_img/dat_014.jpg"
 
-    with open(path, "rb") as image_file:
-        content = image_file.read()
+    # Khởi tạo pipeline với YOLO model
+    pipeline = YOLOGoogleVisionPipeline(yolo_model_path="../models/text_detector.pt", conf=0.25, processor=processor)
 
-    image = vision.Image(content=content)
+    print("Running YOLO + Google Vision OCR pipeline...")
+    final_image, results = pipeline.run(test_image, show=False)
 
-    response = client.text_detection(image=image)
-    texts = response.text_annotations
-
-    if texts:
-        print("Detected text:")
-        print(texts[0].description)  # full text
+    if results:
+        print("\nProcessing completed successfully!")
+        print("Kết quả OCR:")
+        for r in results:
+            print(r)
     else:
-        print("No text detected.")
-
-    if response.error.message:
-        raise Exception(f"{response.error.message}")
-
-# Test
-detect_text("")
+        print("No detections found!")
